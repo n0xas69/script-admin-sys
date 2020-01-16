f291e353f34cf601001dda277d15e4fe91cf625c
n0xas <romain.dusson@gmail.com>
2020-01-16 17:13:04 +0100

    Update test.py

--- test.py
+++ test.py
@@ -2,24 +2,27 @@ import os
 import shutil as sh
 import subprocess as sub
 
-CHEMIN = "C:\Windows\Temp"
+CHEMINS = ["C:\Windows\Temp", r"C:\test", r"%LOCALAPPDATA%\Temp"]
 Ports = [103, 104, 105, 106, 107, 108]
 
 def delete_temp():
-    verif_folder = os.path.exists(CHEMIN)
-    if (verif_folder == True):
-        sh.rmtree(CHEMIN,ignore_errors=True)
-    else:
-        print("Le dossier n'existe pas")
+    for chemin in CHEMINS:
+        verif_folder = os.path.exists(chemin)
+        if (verif_folder == True):
+            sh.rmtree(chemin,ignore_errors=True)
+            try:
+                os.mkdir(chemin)
+            except FileExistsError:
+                pass
+            print("Le dossier ",chemin,"a été vidé")
+        else:
+            print("Le dossier ",chemin," n'existe pas")
 
 def firewall():
     check_firewall = sub.check_output(["whoami"])
-    if "Actif" in check_firewall:
-        print("Le parfeu est désactivé")
 
 # Appel des fonctions
-
-
+delete_temp()
 
 
 
