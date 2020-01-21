2c1bbd3b141a6a90a40d3bff325386013a524794
neiwak <romain.dusson@gmail.com>
2020-01-21 16:25:49 +0100

    ajout dossier scapy

---
+++ scapy/sniffer.py
@@ -0,0 +1,11 @@
+"""
+Utilisation de scapy pour savoir si des requêtes sur le port spécifié arrive,
+création d'une classe paquet qui peut :
+- envoi de paquet
+- sniffage de paquet
+
+fonction rapide :
+envoi d'une requête HTTP
+envoi d'une requête DICOM
+envoi d'une requête 
+"""
