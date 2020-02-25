ce52f67269de49c157c70b14a24f8d2e4cc91e3d
neiwak <romain.dusson@gmail.com>
2020-02-25 14:29:27 +0100

    Update main.py

--- zip_sql_backup/main.py
+++ zip_sql_backup/main.py
@@ -21,21 +21,25 @@ class Backup:
         self.sql_instance = conf["SQL_instance"]
         self.sql_login = conf["SQL_login"]
         self.sql_password = conf["SQL_password"]
+        self.backupD = os.path.join(self.backup_path, "Backup_D")
+        self.backupW = os.path.join(self.backup_path, "Backup_W")
+        self.backupM = os.path.join(self.backup_path, "Backup_M")
+   
     
     # méthode qui définit dans quels dossiers faire le backup
     def location_folder(self):
         dt = datetime.datetime.today()
         backupLocation = []
-        if dt.day == 1:
-            backupLocation.append(os.path.join(self.backup_path, "Backup_M"))
-            backupLocation.append(os.path.join(self.backup_path, "Backup_D"))
+        if dt.day == 25:
+            backupLocation.append(self.backupM)
+            backupLocation.append(self.backupD)
 
         elif dt.weekday == 6:
-            backupLocation.append(os.path.join(self.backup_path, "Backup_W"))
-            backupLocation.append(os.path.join(self.backup_path, "Backup_D"))
+            backupLocation.append(self.backupW)
+            backupLocation.append(self.backupD)
 
         else:
-            backupLocation.append(os.path.join(self.backup_path, "Backup_D"))
+            backupLocation.append(self.backupD)
 
         return backupLocation
   
@@ -51,6 +55,8 @@ class Backup:
         on trie la liste par les valeurs des dictionnaires,
         on supprime le fichier du premier élément de la liste
         """
+        checkFolder = [self.backupD, self.backupW, self.backupM]
+        
         
         list_files = os.listdir(self.backup_path)
         nb_file = len(list_files)
@@ -73,16 +79,27 @@ class Backup:
 
     def zip_backup(self, backupLocation):
         print("backup en cours")
-        if not os.path.exists(self.backup_path + "\Backup_D"):
-            os.makedirs(self.backup_path + "\Backup_D")
-        if not os.path.exists(self.backup_path + "\Backup_W"):
-            os.makedirs(self.backup_path + "\Backup_W")
-        if not os.path.exists(self.backup_path + "\Backup_M"):
-            os.makedirs(self.backup_path + "\Backup_M")
+        if not os.path.exists(self.backupD):
+            os.makedirs(self.backupD)
+        if not os.path.exists(self.backupW):
+            os.makedirs(self.backupW)
+        if not os.path.exists(self.backupM):
+            os.makedirs(self.backupM)
 
         if len(backupLocation) == 2:
             # Backup dans dans le dossier d'indice 0 puis copie du dossier dans l'index 1 de la liste
-            print("OK")
+            zipfileToday = os.path.join(backupLocation[0], date+".zip")
+            if os.path.exists(zipfileToday) == True:
+                os.remove(zipfileToday)
+                
+
+            for file in os.listdir(self.data_path):
+                path = os.path.join(self.data_path, file)
+                with ZipFile(zipfileToday, "a") as zipf:
+                    zipf.write(path)
+            
+            shutil.copy(zipfileToday, backupLocation[1])
+
         else:
             # Backup dans le dossier d'indice 0 de la liste
             zipfileToday = os.path.join(backupLocation[0], date+".zip")
@@ -102,8 +119,8 @@ class Backup:
 
 
 if __name__ == "__main__":
-    test = Backup()
-    backupLocation = test.location_folder()
-    test.zip_backup(backupLocation)
+    backup = Backup()
+    backupLocation = backup.location_folder()
+    backup.zip_backup(backupLocation)
 
 
