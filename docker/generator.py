5e946faa3002e09cd5533da2e872851509f89b96
root <romain.dusson@gmail.com>
2020-08-18 20:59:14 +0000

    update

--- docker/generator.py
+++ docker/generator.py
@@ -1,6 +1,7 @@
 # Generate some containers with ssh for test environment
 
 import argparse
+import subprocess as cmd
 
 parser = argparse.ArgumentParser()
 parser.add_argument("type", help="ubuntu, debian or centos")
@@ -8,3 +9,18 @@ parser.add_argument("number", help="number of containers to deploy", type=int)
 args = parser.parse_args()
 
 print(args.type)
+
+if args.type == "debian":
+    cmd.run("docker pull noxas69/linux_ssh:debian10", shell=True)
+    for c in range(args.number):
+        cmd.run(f"docker run -d --name debian{c} noxas69/linux_ssh:debian10", shell=True)
+        cmd.run(f"docker inspect debian{c} | grep '\"IPAddress\"' | head -n 1", shell=True)
+        
+
+elif args.type == "ubuntu":
+    cmd.run("docker pull noxas69/linux_ssh:ubuntu16.04", shell=True)
+    for c in range(args.number):
+        cmd.run(f"docker run -d --name ubuntu{c} noxas69/linux_ssh:ubuntu16.04", shell=True)
+        cmd.run(f"docker inspect ubuntu{c} | grep '\"IPAddress\"' | head -n 1", shell=True)
+else:
+    print("Enter a valid name of linux ditrib (ubuntu, debian, centos)")
